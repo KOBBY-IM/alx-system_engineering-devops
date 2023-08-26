f54e2d9eb8261ed3b6651ba900e1516ccedb5277
KOBBY-IM <nyarkotheo2@gmail.com>
2023-08-26 09:34:27 +0000

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,14 @@
 # Puppet installer
 
-package { 'puppet-lint':
-    ensure   => '2.1.0',
-    provider => 'flask',
+node 'all' {
+  # Install pip3
+  package { 'python3-pip':
+    ensure => installed,
+  }
+
+  # Install Flask
+  exec { 'install_flask':
+    command => 'pip3 install flask==2.1.0',
+    path => ['/usr/bin/'],
+  }
 }
